3ecba786e574bab7f650ac220be861b7466a8444
Pitt-elias <pedrocancino@gmail.com>
2026-02-24 21:57:40 -0500

    Fase 0 completada - Firebase configurado correctamente

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,28 @@
-// Importa el paquete base de Flutter para usar widgets visuales
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-// Importamos la pantalla de login
-// OJO: la UI siempre se importa desde presentation
 import 'features/auth/presentation/view/login_page.dart';
 
-// Función principal de toda app Flutter
-void main() {
-  // runApp inicia la aplicación y dibuja el primer widget
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
-// MyApp es el widget raíz de toda la aplicación
+
 class MyApp extends StatelessWidget {
-  // Constructor constante (mejora rendimiento)
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // MaterialApp configura cosas globales de la app
     return const MaterialApp(
-      // Quita el banner rojo de debug
       debugShowCheckedModeBanner: false,
-
-      // home es la primera pantalla que se muestra
       home: LoginPage(),
     );
   }
 }
-
 
